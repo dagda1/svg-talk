59989ba0c2d6cebe2529cfb2723c737f745a4d7b
Paul Cowan <paul.cowan@cutting.scot>
2022-12-30 09:35:27 +0000

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,86 @@ import { Rects } from './components/Rects/Rects';
 import { useToggle } from './hook/useToggle';
 import { useRef } from 'react';
 import { useParentSize } from '@cutting/use-get-parent-size';
+import { GridRows, GridColumns } from '@visx/grid';
+import { scaleLinear } from '@visx/scale';
+import cs from 'classnames';
 
 export function App(): JSX.Element {
   const [showViewport, setViewport] = useToggle();
-  const [, setSvgViewport] = useToggle();
+  const [showSvgViewport, setSvgViewport] = useToggle();
+  const [showViewbox, setViewbox] = useToggle();
   const containerRef = useRef<HTMLDivElement>(null);
-  const { width, height } = useParentSize(containerRef, { debounceDelay: 100 });
+  const { width, height } = useParentSize(containerRef, { debounceDelay: 1000 });
 
-  console.log(width, height);
+  const aspect = width / height;
+
+  const adjustedHeight = Math.ceil(width / aspect);
+
+  console.dir({ width, height, adjustedHeight, aspect });
+
+  const svgXscale = scaleLinear({
+    domain: [0, 10],
+    range: [0, width],
+    round: true,
+  });
+
+  const svgYscale = scaleLinear({
+    domain: [0, 10],
+    range: [height, 0],
+    round: true,
+  });
+
+  const viewboxScale = scaleLinear({
+    domain: [0, 10],
+    range: [0, width],
+    round: true,
+  });
+
+  const viewBoxYScale = scaleLinear({
+    domain: [0, 10],
+    range: [height, 0],
+    round: true,
+  });
+
   return (
     <>
-      <ApplicationLayout center theme="salesTheme">
-        <header>
-          <Button type="button" buttonStyle="secondary" onClick={setViewport}>
-            Browser Viewport
-          </Button>
-          <Button type="button" buttonStyle="secondary" onClick={setSvgViewport}>
-            SVG Viewport
-          </Button>
-          <Button type="button" buttonStyle="secondary" onClick={setSvgViewport}>
-            viewbox
-          </Button>
-        </header>
-        <div className="svg-container" ref={containerRef}>
-          <Rects width={width} height={height} />
-        </div>
+      <ApplicationLayout
+        center
+        theme="salesTheme"
+        innerRef={containerRef}
+        className={cs({ grid: showViewport })}
+        header={
+          <>
+            <Button type="button" buttonStyle="secondary" onClick={setViewport}>
+              Browser Viewport
+            </Button>
+            <Button type="button" buttonStyle="secondary" onClick={setSvgViewport}>
+              SVG Viewport
+            </Button>
+            <Button type="button" buttonStyle="secondary" onClick={setViewbox}>
+              viewbox
+            </Button>
+          </>
+        }
+      >
+        <>
+          <svg style={{ overflow: 'visible' }} height={height} width={width}>
+            <Rects height={height} width={width} />
+            {showSvgViewport && (
+              <>
+                <GridRows scale={svgXscale} width={width} height={height} stroke="orange" />
+                <GridColumns scale={svgYscale} width={width} height={height} stroke="orange" />
+              </>
+            )}
+            {showViewbox && (
+              <>
+                <GridRows scale={viewboxScale} width={width} height={adjustedHeight} stroke="yellow" />
+                <GridColumns scale={viewBoxYScale} width={width} height={adjustedHeight} stroke="yellow" />
+              </>
+            )}
+          </svg>
+        </>
       </ApplicationLayout>
-      {showViewport && (
-        <div className="grid" ref={containerRef}>
-          <div>
-            <h2>Browser Viewport</h2>
-          </div>
-        </div>
-      )}
     </>
   );
 }
--- src/components/Rects/Rects.tsx
+++ src/components/Rects/Rects.tsx
@@ -1,32 +1,23 @@
-import { GridRows, GridColumns } from '@visx/grid';
-
-interface RectsProps {
-  width: number;
-  height: number;
-}
-
-export function Rects({ width, height }: RectsProps): JSX.Element {
+export function Rects({ width, height }: { width: number; height: number }): JSX.Element {
   return (
-    <svg style={{ height: '100%', width: '100%' }}>
-      {/* <GridRows scale={temperatureScale} width={xMax} height={yMax} stroke="#e0e0e0" />
-      <GridColumns scale={timeScale} width={xMax} height={yMax} stroke="#e0e0e0" /> */}
+    <>
       <rect
-        x="20%"
-        y="20%"
-        width="100%"
-        height="100%"
+        x="0"
+        y="0"
+        width={width}
+        height={height}
         rx="20"
         style={{ fill: '#ff0000', stroke: '#000000', strokeWidth: '2px' }}
       />
 
       <rect
-        x="10%"
-        y="10%"
-        width="100%"
-        height="100%"
-        rx="40"
+        x="50"
+        y="50"
+        width={width}
+        height={height}
+        rx="20"
         style={{ fill: '#0000ff', stroke: '#000000', strokeWidth: '2px', fillOpacity: 0.7 }}
       />
-    </svg>
+    </>
   );
 }
