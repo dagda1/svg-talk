8f0b914d27c0479c7ac95fdb85acd2ed4af0e9e1
Paul Cowan <paul.cowan@cutting.scot>
2023-02-21 06:59:08 +0000

    add util

--- src/components/SVG/SVG.tsx
+++ src/components/SVG/SVG.tsx
@@ -1,5 +1,6 @@
 import { useParentSize } from '@cutting/use-get-parent-size';
-import { useRef } from 'react';
+import { scaleLinear } from '@visx/scale';
+import { useMemo, useRef } from 'react';
 import { Grids } from './Grids';
 
 interface SVGProps {
@@ -14,18 +15,43 @@ export function SVG({ showSvgViewport, showViewbox }: SVGProps): JSX.Element {
   const viewBoxWidth = width;
   const viewBoxHeight = height;
 
+  const { radius } = useMemo(() => {
+    const xScale = scaleLinear({ domain: [0, 20], range: [0, width] });
+    const yScale = scaleLinear({ domain: [0, 20], range: [height, 0] });
+
+    const radius = yScale(12);
+
+    return { radius, xScale, yScale };
+  }, [width, height]);
+
+  console.log(radius);
   return (
     <div ref={containerRef} className="container">
-      <svg style={{ overflow: 'visible' }}>
-        <Grids
-          width={width}
-          height={height}
-          showSvgViewport={showSvgViewport}
-          showViewbox={showViewbox}
-          viewBoxWidth={viewBoxWidth}
-          viewBoxHeight={viewBoxHeight}
-        />
-      </svg>
+      {/* <svg width="500" height="100" viewBox="0 0 500 10" preserveAspectRatio="xMinYMin meet"> */}
+      <div
+        style={{
+          display: 'inline-block',
+          position: 'relative',
+          width: '100%',
+          verticalAlign: 'top',
+          overflow: 'hidden',
+          border: '10px solid red',
+        }}
+      >
+        <svg viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="xMinYMin meet">
+          <g>
+            <circle r={radius} cx={width / 2} cy={height / 2} fill="#f00" />
+          </g>
+          <Grids
+            width={width}
+            height={height}
+            showSvgViewport={showSvgViewport}
+            showViewbox={showViewbox}
+            viewBoxWidth={viewBoxWidth}
+            viewBoxHeight={viewBoxHeight}
+          />
+        </svg>
+      </div>
     </div>
   );
 }
